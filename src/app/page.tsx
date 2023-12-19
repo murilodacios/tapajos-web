f5a1ffcf248aa61335e108d1ebecb4f8e2fb1a47
Murilo Dácio <murilodacio@gmail.com>
2023-12-19 13:28:28 -0300

    Initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,129 @@
-import Image from 'next/image'
+'use client';
+
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import axios from 'axios'
+import { useState } from 'react';
+import convert from 'xml-js'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { useToast } from '@/components/ui/use-toast';
+import { Toaster } from '@/components/ui/toaster';
+
+
 
 export default function Home() {
+
+  const api = axios.create({
+    baseURL: "http://localhost:3456",
+  })
+
+  const { toast } = useToast()
+
+  const [procedimento, setProcedimento] = useState()
+  const [numProcesso, setNumProcesso] = useState("")
+
+  const consultaProcesso = async () => {
+    api.post("/consultar", {
+      processo: numProcesso
+    }).then((response) => {
+      var resultado = convert.xml2json(response.data, { compact: true, spaces: 4, sanitize: true, trim: true });
+
+      var resultadoParse = JSON.parse(resultado)
+
+      setProcedimento(resultadoParse)
+
+      toast({
+        title: "Processo carregado",
+      })
+    }).catch((err) => {
+      toast({
+        title: "Este processo não existe",
+        variant: "destructive"
+      })
+    })
+
+
+  }
+
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <main className="max-w-5xl m-auto flex flex-col items-center p-4 md:p-12 xs:p-4">
+
+      <Toaster />
+
+      <div className='w-full flex flex-col gap-4'>
+        <p>Consulte processos abertos no Sistema Eletrônico de Informações (SEI) da Prefeitura Municipal de Itaboraí</p>
+        <Input className='w-full' placeholder='Digite o número do processo com todas as pontuações' onChange={(e) => setNumProcesso(e.target.value)} />
+        <Button onClick={consultaProcesso}>Consultar processo</Button>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+      {procedimento &&
+        <>
+          <Card className='w-full mt-4'>
+            <CardHeader>
+              <CardTitle>Informações do processo</CardTitle>
+              <CardDescription>{procedimento["SOAP-ENV:Envelope"]["SOAP-ENV:Body"]["ns1:consultarProcedimentoResponse"]["parametros"]["TipoProcedimento"]["Nome"]["_text"]}
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <div className='flex flex-col gap-0'>
+                <p>
+                  <strong>{procedimento["SOAP-ENV:Envelope"]["SOAP-ENV:Body"]["ns1:consultarProcedimentoResponse"]["parametros"]["ProcedimentoFormatado"]["_text"]}</strong>
+                </p>
+                <p className='text-sm'>
+                  {procedimento["SOAP-ENV:Envelope"]["SOAP-ENV:Body"]["ns1:consultarProcedimentoResponse"]["parametros"]["Especificacao"]["_text"]}
+                </p>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+                <div className="flex flex-row gap-2">
+                  <p className='text-sm'>
+                    Data de autuação:
+                  </p>
+                  <p className='text-sm'>
+                    {procedimento["SOAP-ENV:Envelope"]["SOAP-ENV:Body"]["ns1:consultarProcedimentoResponse"]["parametros"]["DataAutuacao"]["_text"]}
+                  </p>
+                </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+              </div>
+            </CardContent>
+            <CardFooter className='flex-col items-start'>
+
+              <strong> <p>Último andamento do processo</p></strong>
+
+
+
+              <p>
+                {procedimento["SOAP-ENV:Envelope"]["SOAP-ENV:Body"]["ns1:consultarProcedimentoResponse"]["parametros"]["UltimoAndamento"]["DataHora"]["_text"]}
+              </p>
+
+              <p>
+                {procedimento["SOAP-ENV:Envelope"]["SOAP-ENV:Body"]["ns1:consultarProcedimentoResponse"]["parametros"]["UltimoAndamento"]["Descricao"]["_text"]}
+              </p>
+
+              <div className='flex flex-col sm:flex-row gap-2 text-sm'>
+                <p>Unidade: </p>
+                <p>
+                  {procedimento["SOAP-ENV:Envelope"]["SOAP-ENV:Body"]["ns1:consultarProcedimentoResponse"]["parametros"]["UltimoAndamento"]["Unidade"]["Descricao"]["_text"]} -
+                </p>
+                <p>
+                  {procedimento["SOAP-ENV:Envelope"]["SOAP-ENV:Body"]["ns1:consultarProcedimentoResponse"]["parametros"]["UltimoAndamento"]["Unidade"]["Sigla"]["_text"]}
+                </p>
+              </div>
+
+            </CardFooter>
+          </Card>
+        </>
+      }
+
+
     </main>
   )
 }
